5ee9cd717262e46312b92d36a045aa0f7951dacc
TomcikKometa <tomcik48_84@o2.pl>
2024-09-20 22:49:14 +0200

    add rxAngular librery"

---
+++ src/app/@config/form-config.ts
@@ -0,0 +1,12 @@
+import { MatDialogConfig } from '@angular/material/dialog';
+
+export const DIALOG_OPTIONS_POP_UP: MatDialogConfig = {
+  width: '28vw',
+  height:'24vw',
+  enterAnimationDuration: '500ms',
+  exitAnimationDuration: '500ms',
+  position: {
+    top: '100px'
+  },
+  autoFocus:false
+};
