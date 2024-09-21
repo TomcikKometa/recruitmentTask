4177fda41dc0a4a89976ca563f98795bd4f3660a
TomcikKometa <tomcik48_84@o2.pl>
2024-09-22 01:01:38 +0200

    develop periodic state srevice

--- src/app/features/services/pop-up-form.service.ts
+++ src/app/features/services/pop-up-form.service.ts
@@ -1,43 +1,44 @@
 import { inject, Injectable } from '@angular/core';
-import { NonNullableFormBuilder, FormGroup, Validators, FormControl, Form } from '@angular/forms';
+import { NonNullableFormBuilder, FormGroup, Validators, FormControl, Form, FormBuilder } from '@angular/forms';
 import {
   NameForm,
   PeriodicElementForm,
   PopUpData,
+  PopUpGroup,
   PositionForm,
   SymbolForm,
   WeightForm
 } from '../periodic-elements/components/popup/model/pop-up-form';
-import {
-  PeriodicElementControlType,
-  PeriodicElementEnum
-} from '../periodic-elements/components/popup/enum/popup-form-enum';
-import { PeriodicElement } from '../../@api/models/periodicElement';
+import { PeriodicElementControlType } from '../periodic-elements/components/popup/enum/popup-form-enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PopUpFormService {
-  public _popUpForm!: FormGroup;
+  private _popUpForm?: FormGroup;
 
-  constructor(private readonly formBuilder: NonNullableFormBuilder) {}
+  private readonly formBuilder: NonNullableFormBuilder = inject(NonNullableFormBuilder);
 
-  public fillPopUpForm(popUpData: PopUpData): FormGroup | undefined {
+  public get popUpForm(): FormGroup<PopUpGroup> | undefined {
+    return this._popUpForm;
+  }
+
+  public prepareForm(popUpData: PopUpData): FormGroup<PopUpGroup> | undefined {
     switch (popUpData.editType) {
       case PeriodicElementControlType.POSITION:
-        return (this._popUpForm = this.formBuilder.group<PositionForm>({
+        return (this._popUpForm = this.formBuilder.group<PopUpGroup>({
           [PeriodicElementControlType.POSITION]: this.formBuilder.control<number>(popUpData.element.position)
         }));
       case PeriodicElementControlType.NAME:
-        return (this._popUpForm = this.formBuilder.group<NameForm>({
+        return (this._popUpForm = this.formBuilder.group<PopUpGroup>({
           [PeriodicElementControlType.NAME]: this.formBuilder.control<string>(popUpData.element.name)
         }));
       case PeriodicElementControlType.WEIGHT:
-        return (this._popUpForm = this.formBuilder.group<WeightForm>({
+        return (this._popUpForm = this.formBuilder.group<PopUpGroup>({
           [PeriodicElementControlType.WEIGHT]: this.formBuilder.control<number>(popUpData.element.weight)
         }));
       case PeriodicElementControlType.SYMBOL:
-        return (this._popUpForm = this.formBuilder.group<SymbolForm>({
+        return (this._popUpForm = this.formBuilder.group<PopUpGroup>({
           [PeriodicElementControlType.SYMBOL]: this.formBuilder.control<string>(popUpData.element.symbol)
         }));
       default:
