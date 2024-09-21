011b558a5537b3ff5d35f033ac44458573aa742f
TomcikKometa <tomcik48_84@o2.pl>
2024-09-22 01:00:09 +0200

    develop perdidic table

--- src/app/features/periodic-elements/components/popup/popup.component.ts
+++ src/app/features/periodic-elements/components/popup/popup.component.ts
@@ -1,8 +1,8 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogActions, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { PopUpData } from './model/pop-up-form';
 import { PopUpFormService } from '../../../services/pop-up-form.service';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -11,26 +11,38 @@ import { PeriodicElementControlType } from './enum/popup-form-enum';
 @Component({
   selector: 'app-popup',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatDialogActions],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatDialogClose, MatInputModule, MatButtonModule, MatDialogActions,MatDialogModule],
   templateUrl: './popup.component.html',
   styleUrl: './popup.component.scss'
 })
 export class PopupComponent implements OnInit {
-  protected popUpForm!: FormGroup | undefined;
+  protected popUpForm?: FormGroup;
+  protected readonly periodicElementControlType: typeof PeriodicElementControlType = PeriodicElementControlType;
+  protected controlType: string = '';
+  protected inputType: 'text' | 'number' = 'text';
+  protected label: string = '';
 
   protected readonly matDialogRef: MatDialogRef<PopupComponent> = inject(MatDialogRef);
   protected readonly data: PopUpData = inject(MAT_DIALOG_DATA);
   private readonly popFormService: PopUpFormService = inject(PopUpFormService);
 
   ngOnInit(): void {
-    this.popUpForm = this.popFormService.fillPopUpForm(this.data);
-  }
-
-  protected get periodicElementControlType(): typeof PeriodicElementControlType {
-    return PeriodicElementControlType;
+    this.data ? this.popUpForm = this.popFormService.prepareForm(this.data) : '';
+    switch (this.data.editType) {
+      case PeriodicElementControlType.NAME:
+        this.controlType = PeriodicElementControlType.NAME;
+        this.inputType = 'text';
+        this.label = 'Name';
+        break;
+      case PeriodicElementControlType.POSITION:
+        this.controlType = PeriodicElementControlType.POSITION;
+        this.inputType = 'number';
+        this.label = 'Position';
+        break;
+    }
   }
 
   protected save(): void {
-    this.matDialogRef.close(this.popUpForm?.value)
+    this.matDialogRef.close(this.popUpForm?.value);
   }
 }
